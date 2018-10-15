879d234761d158c9582b761b08ed14ebaf302f55
waterproofpatch <waterproofpatch@gmail.com>
2018-10-14 20:09:51 -0400

    created main

--- src/server/server.c
+++ src/server/server.c
@@ -13,10 +13,16 @@
 #include "protocol.h"
 #include "server.h"
 
+int    g_client_sockets[MAX_CLIENTS];   // each client's fd
+int g_connected_clients = 0; // number of currently connected clients
+int    g_max_fd;                        // max file descriptor
+int    g_server_sock_fd = 0;            // server socket
+    list_t *       g_active_user_list = 0;
+fd_set g_all_fds;                       // set file descriptors
 
 /**
  * @brief print a user object via list_foreach
- * 
+ *
  * @param user_opaque the user passed in via list_foreach
  * @param context context provided to list_foreach
  */
@@ -26,19 +32,14 @@ static void print_user(void *user_opaque, void *context)
     DBG_INFO("username %s, socket %d\n", user->name, user->client_socket_fd);
 }
 
-/**
- * @brief create a server
- * @param port_no: the port to listen on
- * @param sock_fd_out: ptr to socket to write with created server socket
- * @return: OK if server socket created successfully, ERR_* otherwise. See
- * error_codes.h.
- * */
-static proto_err_t create_server(unsigned short port_no, int *sock_fd_out);
-
-int    g_client_sockets[MAX_CLIENTS];   // each client's fd
-int    g_max_fd;                        // max file descriptor
-int    g_server_sock_fd = 0;            // server socket
-fd_set g_all_fds;                       // set file descriptors
+int server_get_client_socket(int index)
+{
+    if (index >= MAX_CLIENTS)
+    {
+        return -1;
+    }
+    return g_client_sockets[index];
+}
 
 static char server_client_socket_fd_comparator(void *context, void *key)
 {
@@ -69,10 +70,15 @@ static void server_add_client_socket(int socket_fd)
     }
 }
 
-static proto_err_t create_server(unsigned short port_no, int *sock_fd_out)
+proto_err_t server_create(unsigned short port_no)
 {
     int                sock_fd   = 0;
     struct sockaddr_in serv_addr = {0};
+
+    server_init_client_sockets();
+
+    // initialize a user list to contain list of active users
+    g_active_user_list = list_init(malloc, free);
 
     // create an 'INTERNET' STREAM socket
     sock_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -114,11 +120,12 @@ static proto_err_t create_server(unsigned short port_no, int *sock_fd_out)
         return ERR_NETWORK_FAILURE;
     }
 
-    *sock_fd_out = sock_fd;
+    g_server_sock_fd = sock_fd;
     return OK;
 }
 
-static proto_err_t process_new_client(int g_server_sock_fd, list_t *active_user_list)
+static proto_err_t process_new_client(int     g_server_sock_fd,
+                                      list_t *g_active_user_list)
 {
     proto_err_t        status         = OK;
     socklen_t          client_length  = sizeof(struct sockaddr_in);
@@ -139,7 +146,7 @@ static proto_err_t process_new_client(int g_server_sock_fd, list_t *active_user_
     if (status != OK)
     {
         DBG_ERROR("Unable to request client name: %s\n",
-               PROTO_ERR_T_STRING[status]);
+                  PROTO_ERR_T_STRING[status]);
         close(new_sock_fd);
         status = ERR_GENERAL;
         goto done;
@@ -150,12 +157,13 @@ static proto_err_t process_new_client(int g_server_sock_fd, list_t *active_user_
     status         = proto_read_client_name(new_sock_fd, &name_out);
     if (status != OK)
     {
-        DBG_ERROR("Unable to read client name: %s\n", PROTO_ERR_T_STRING[status]);
+        DBG_ERROR("Unable to read client name: %s\n",
+                  PROTO_ERR_T_STRING[status]);
         status = proto_disconnect_client(new_sock_fd, "invalid client name");
         if (status != OK)
         {
             DBG_ERROR("Unable to disconnect client: %s\n",
-                   PROTO_ERR_T_STRING[status]);
+                      PROTO_ERR_T_STRING[status]);
             goto done;
         }
         status = ERR_GENERAL;
@@ -176,7 +184,7 @@ static proto_err_t process_new_client(int g_server_sock_fd, list_t *active_user_
     memcpy(new_user->name, name_out, strlen(name_out));
     free(name_out);
     user_t *existing_user = (user_t *)list_search(
-        active_user_list, user_comparator, new_user->name);
+        g_active_user_list, user_comparator, new_user->name);
     if (existing_user)
     {
         DBG_INFO("User with name %s already exists, kicking\n", new_user->name);
@@ -192,10 +200,10 @@ static proto_err_t process_new_client(int g_server_sock_fd, list_t *active_user_
     }
 
     DBG_INFO("Added user %s, socket %d to active user list.\n", new_user->name,
-           new_user->client_socket_fd);
-    list_add(active_user_list, new_user);
+             new_user->client_socket_fd);
+    list_add(g_active_user_list, new_user);
     DBG_INFO("User list is now:\n");
-    list_foreach(active_user_list, print_user, NULL);
+    list_foreach(g_active_user_list, print_user, NULL);
     server_add_client_socket(new_user->client_socket_fd);
     status = OK;
 
@@ -222,125 +230,91 @@ static void update_max_fd()
     }
 }
 
-int main(int argc, char *argv[])
+void server_handle_connections()
 {
-    // we'll listen on a high port to avoid having to sudo
-    proto_err_t    status           = OK;
-    unsigned short port_no          = 5001;
-    list_t *       active_user_list = 0;
+    update_max_fd();
 
-    // init the socket set for the clients
-    server_init_client_sockets();
-
-    // init the client socket client socket handler
-    // pthread_create(&g_client_handler_thread, NULL,
-    // server_handle_client_thread, NULL);
-
-    // create the server
-    DBG_INFO("server running on port %d\n", port_no);
-    status = create_server(port_no, &g_server_sock_fd);
-    if (status != OK)
+    int num_ready_fd = select(g_max_fd + 1, &g_all_fds, NULL, NULL, NULL);
+    if (num_ready_fd < 0 && errno != EINTR)
     {
-        DBG_INFO("Unable to create server: %s\n", PROTO_ERR_T_STRING[status]);
-        return -1;
+        DBG_INFO("Select error %d\n", errno);
+        return;
     }
 
-    // initialize a user list to contain list of active users
-    active_user_list = list_init(malloc, free);
+    // TODO maybe need while num_ready_fd here, so we service each ready fd
+    // before resetting them all in update_max_fd
 
-    // wait for a client to connect
-    int connected_clients = 0;
-
-    while (1)
+    // if the activity was on our listening socket, it's a new connection
+    if (FD_ISSET(g_server_sock_fd, &g_all_fds))
     {
-        // recalculate the maximum FD for our call to select
-        update_max_fd();
-
-        int num_ready_fd = select(g_max_fd + 1, &g_all_fds, NULL, NULL, NULL);
-        if (num_ready_fd < 0 && errno != EINTR)
+        if (g_connected_clients == MAX_CLIENTS)
         {
-            DBG_INFO("Select error %d\n", errno);
+            DBG_INFO("Cannot accept any new clients!\n");
+            return;
+        }
+        else
+        {
+            DBG_INFO("Processing new connection...\n");
+            if (process_new_client(g_server_sock_fd, g_active_user_list) == OK)
+            {
+                g_connected_clients++;
+            }
+            else
+            {
+                DBG_INFO("Error processing new connection...\n");
+            }
+            return;
+        }
+    }
+
+    // otherwise we have a message from an already connected client
+    for (int i = 0; i < MAX_CLIENTS; i++)
+    {
+        int sd = server_get_client_socket(i);
+        // if we don't have any stored fds or if the stored fd isn't the one
+        // who woke us up, continue
+        if (sd == -1 || !FD_ISSET(sd, &g_all_fds))
+        {
             continue;
         }
 
-        // TODO maybe need while num_ready_fd here, so we service each ready fd
-        // before resetting them all in update_max_fd
-
-        // if the activity was on our listening socket, it's a new connection
-        if (FD_ISSET(g_server_sock_fd, &g_all_fds))
+        user_t *user = list_search(g_active_user_list,
+                                   server_client_socket_fd_comparator, &sd);
+        if (!user)
         {
-            if (connected_clients == MAX_CLIENTS)
-            {
-                DBG_INFO("Cannot accept any new clients!\n");
-                continue;
-            }
-            else
-            {
-                DBG_INFO("Processing new connection...\n");
-                if (process_new_client(g_server_sock_fd, active_user_list) ==
-                    OK)
-                {
-                    connected_clients++;
-                }
-                else
-                {
-                    DBG_INFO("Error processing new connection...\n");
-                }
-                continue;
-            }
+            DBG_INFO(
+                "Someone is talking on socket %d, but I don't know "
+                "who...\n",
+                sd);
+            return;   // this is a problem
         }
+        DBG_INFO("User [%s] has a message for the server.\n", user->name);
 
-        // otherwise we have a message from an already connected client
-        for (int i = 0; i < MAX_CLIENTS; i++)
+        command_t * cmd    = NULL;
+        proto_err_t status = OK;
+        status             = proto_read_command(sd, &cmd);
+        if (status != OK || cmd->command_type == CMD_REQUEST_DISCONNECT)
         {
-            int sd = g_client_sockets[i];
-            // if we don't have any stored fds or if the stored fd isn't the one
-            // who woke us up, continue
-            if (sd == -1 || !FD_ISSET(sd, &g_all_fds))
-            {
-                continue;
-            }
-
-            DBG_INFO("searching user list:\n");
-            list_foreach(active_user_list, print_user, NULL);
-            user_t *user = list_search(active_user_list,
-                                       server_client_socket_fd_comparator, &sd);
-            if (!user)
-            {
-                DBG_INFO(
-                    "Someone is talking on socket %d, but I don't know "
-                    "who...\n",
-                    sd);
-                return -1;   // this is a problem
-            }
-            DBG_INFO("User [%s] has a message for the server.\n", user->name);
-
-            command_t * cmd    = NULL;
-            proto_err_t status = OK;
-            status             = proto_read_command(sd, &cmd);
-            if (status != OK || cmd->command_type == CMD_REQUEST_DISCONNECT)
-            {
-                DBG_INFO("Status is %s, disconnecting client %s\n",
-                       PROTO_ERR_T_STRING[status], user->name);
-                g_client_sockets[i] = -1;
-                connected_clients--;
-                list_remove(active_user_list, user);
-                close(sd);
-                continue;
-            }
-            else if (cmd->command_type == CMD_REQUEST_USERLIST_FROM_SERVER)
-            {
-                DBG_INFO("User [%s] requests user list from server...\n",
-                       user->name);
-                proto_send_user_list(sd, active_user_list);
-            }
-            else
-            {
-                DBG_INFO("User [%s] send a command:\n", user->name);
-                proto_print_command(cmd);
-            }
+            DBG_INFO("Status is %s, disconnecting client %s\n",
+                     PROTO_ERR_T_STRING[status], user->name);
+            g_client_sockets[i] = -1;
+            g_connected_clients--;
+            list_remove(g_active_user_list, user);
+            close(sd);
+            continue;
         }
-        DBG_INFO("Done\n");
+        else if (cmd->command_type == CMD_REQUEST_USERLIST_FROM_SERVER)
+        {
+            DBG_INFO("User [%s] requests user list from server...\n",
+                     user->name);
+            proto_send_user_list(sd, g_active_user_list);
+        }
+        else
+        {
+            DBG_INFO("User [%s] send a command:\n", user->name);
+            proto_print_command(cmd);
+        }
     }
-    return 0;
+    DBG_INFO("Done\n");
+    return;
 }
--- src/server/server.h
+++ src/server/server.h
@@ -1,6 +1,8 @@
 #ifndef __SERVER_H_
 #define __SERVER_H_
 
+#include "protocol.h"
+
 #ifndef MAX
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #endif
@@ -17,5 +19,19 @@
         fprintf(stderr, "%s:%d:%s ERROR: " fmt, __FILE__, __LINE__, __func__, \
                 ##__VA_ARGS__);                                               \
     } while (0)
+
+/**
+ * @brief call in a loop to handle connections once the server is created.
+ * 
+ */
+void server_handle_connections();
+
+/**
+ * @brief call at the beginning to set up the client sockets and bind the server to the specified port
+ * 
+ * @param port_no the port to bind
+ * @return proto_err_t  OK if the server was started successfully, ERR_* otherwise
+ */
+proto_err_t server_create(unsigned short port_no);
 
 #endif   // __SERVER_H_
