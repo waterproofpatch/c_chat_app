f802213468549aa0637ec0e0ddd1c0844305948d
waterproofpatch <waterproofpatch@gmail.com>
2018-10-19 23:33:43 -0400

    comments

--- src/shared/protocol.h
+++ src/shared/protocol.h
@@ -6,7 +6,8 @@
 #include "list.h"
 #include "error_codes.h"
 
-#define MAX_CLIENTS 10
+#define MAX_CLIENTS 10 // maximum number of clients we allow to be connected
+#define MAX_MESSAGE_LENGTH 3000 // maximum length for a message
 
 // conditional static for test
 #ifndef TEST
