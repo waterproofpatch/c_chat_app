9f5f7007efff0b1c3dc1cacf7903a11a33fc0298
waterproofpatch <waterproofpatch@gmail.com>
2018-10-19 23:33:30 -0400

    null terminating username and message

--- src/shared/protocol.c
+++ src/shared/protocol.c
@@ -204,19 +204,25 @@ proto_err_t proto_broadcast_message(int    sock_fd,
                                     char * message,
                                     size_t message_length)
 {
+    if (message_length > MAX_MESSAGE_LENGTH)
+    {
+        return ERR_PAYLOAD_TOO_LARGE;
+    }
     broadcast_message_t *broadcast_message =
-        wrappers_malloc(sizeof(broadcast_message_t) + message_length);
+        wrappers_malloc(sizeof(broadcast_message_t) + message_length + 1);
 
     if (!broadcast_message)
     {
         return ERR_NO_MEM;
     }
+    wrappers_memset(broadcast_message->name, '\0', MAX_USER_NAME_LENGTH);
+    wrappers_memset(broadcast_message->message, '\0', message_length + 1);
     wrappers_memcpy(broadcast_message->name, name, name_length);
     wrappers_memcpy(broadcast_message->message, message, message_length);
     broadcast_message->message_length = message_length;
     proto_err_t res                   = proto_send_command(
         sock_fd, CMD_RECEIVE_GLOBAL_MESSAGE, (char *)broadcast_message,
-        sizeof(broadcast_message_t) + message_length);
+        sizeof(broadcast_message_t) + message_length + 1);
     wrappers_free(broadcast_message);
     return res;
 }
