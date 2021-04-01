612e05aa5832967445e15c94f66f8d373ac7bc18
waterproofpatch <waterproofpatch@gmail.com>
2021-03-31 21:14:00 -0400

    no root

--- install-deps.sh
+++ install-deps.sh
@@ -5,6 +5,6 @@ curl -sSL https://get.rvm.io | bash -s stable
 rvm install 2.7
 rvm use 2.7 --default
 
-sudo gem install ceedling
-sudo pip3 install gcovr
+gem install ceedling
+pip3 install gcovr
 pip install -r requirements.txt
