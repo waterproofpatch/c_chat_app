e197a3145be46c77f69f7653889807f37f853398
waterproofpatch <waterproofpatch@gmail.com>
2021-04-02 20:58:58 -0400

    3.6

--- install-deps.sh
+++ install-deps.sh
@@ -2,7 +2,6 @@
 
 # apt stuff
 sudo apt update
-sudo apt-get install python3-pip 
 
 # ruby stuff
 curl -sSL https://get.rvm.io | bash -s stable
@@ -11,7 +10,4 @@ rvm use 2.7 --default
 gem install ceedling
 
 # pip stuff
-which pip3
-python3 -m pip --version
-pip3 --version
-pip3 install -r requirements.txt
+python3.6 -m pip --version
